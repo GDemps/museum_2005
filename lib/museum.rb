00aebedc3b78abd995d49bc33b0c9e5cdbd7fbf7
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-27 11:02:47 -0600

    ticket_lottery_contestants done

--- lib/museum.rb
+++ lib/museum.rb
@@ -47,4 +47,15 @@ class Museum
     collection
   end
 
+  def ticket_lottery_contestants(exhibit)
+    lottery_contestants = []
+      @patrons.each do |patron|
+        if patron.spending_money < exhibit.cost
+          lottery_contestants << patron
+        end
+      end
+      lottery_contestants
+  end
+
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -98,4 +98,45 @@ class MuseumTest < Minitest::Test
     assert_equal expected, dmns.patrons_by_exhibit_interest
   end
 
+  def test_ticket_lottery_contestants
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    assert_equal [], dmns.patrons
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    assert_equal [patron_1, patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
+  end
+#
+# dmns.draw_lottery_winner(dead_sea_scrolls)
+#   # => "Johnny" or "Bob" can be returned here. Fun!
+#
+# dmns.draw_lottery_winner(gems_and_minerals)
+#   # => nil
+#
+#   #If no contestants are elgible for the lottery, nil is returned.
+#
+# dmns.announce_lottery_winner(imax)
+#   # => "Bob has won the IMAX edhibit lottery"
+#
+#   # The above string should match exactly, you will need to stub the return of `draw_lottery_winner` as the above method should depend on the return value of `draw_lottery_winner`.
+#
+# dmns.announce_lottery_winner(gems_and_minerals)
+#   # => "No winners for this lottery"
+#
+#   # If there are no contestants, there are no winners.
+#   ```
+
 end
