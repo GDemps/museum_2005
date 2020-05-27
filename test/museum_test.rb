bbcb18185417ffa16062666491bc792da40b3086
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-27 09:56:32 -0600

    test and setup complete for admit method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -59,13 +59,18 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(gems_and_minerals)
     dmns.add_exhibit(dead_sea_scrolls)
     dmns.add_exhibit(imax)
-    patron_1 = Patron.new("Bob", 20)
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
-    patron_2 = Patron.new("Sally", 20)
-    patron_2.add_interest("IMAX")
     assert_equal [], dmns.patrons
-    
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Gems and Minerals")
+        patron_1.add_interest("Dead Sea Scrolls")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+    assert_equal [patron_1, patron_2, patron_3], dmns.patrons
   end
 
 end
