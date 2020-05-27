c962db8b91c8bdd6f7b8acd8166813a6ab89c0be
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-27 09:59:06 -0600

    corrected test name for admit method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -51,7 +51,7 @@ class MuseumTest < Minitest::Test
     assert_equal [imax], dmns.recommend_exhibits(patron_2)
   end
 
-  def test_patrons_by_exhibit_interest
+  def test_patrons_admission
     dmns = Museum.new("Denver Museum of Nature and Science")
     gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
@@ -62,7 +62,7 @@ class MuseumTest < Minitest::Test
     assert_equal [], dmns.patrons
     patron_1 = Patron.new("Bob", 20)
     patron_1.add_interest("Gems and Minerals")
-        patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Dead Sea Scrolls")
     patron_2 = Patron.new("Sally", 20)
     patron_2.add_interest("Dead Sea Scrolls")
     patron_3 = Patron.new("Johnny", 5)
@@ -71,6 +71,10 @@ class MuseumTest < Minitest::Test
     dmns.admit(patron_2)
     dmns.admit(patron_3)
     assert_equal [patron_1, patron_2, patron_3], dmns.patrons
+  end
+
+  def test_patrons_by_exhibit_interest
+
   end
 
 end
