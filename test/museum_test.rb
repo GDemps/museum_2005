89fe86a1cc66e542cac8c340bb835872c6420c70
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-27 09:51:44 -0600

    test created for dmns.patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -51,4 +51,21 @@ class MuseumTest < Minitest::Test
     assert_equal [imax], dmns.recommend_exhibits(patron_2)
   end
 
+  def test_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+    assert_equal [], dmns.patrons
+    
+  end
+
 end
